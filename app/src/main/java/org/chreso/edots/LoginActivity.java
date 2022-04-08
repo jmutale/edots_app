e36a3224fd90fb6cd139d9087d563f0820394895
Jacob Mutale <Jacob.Mutale@cidrz.org>
2022-04-08 07:22:52 +0200

    Added login activity

---
+++ app/src/main/java/org/chreso/edots/LoginActivity.java
@@ -0,0 +1,33 @@
+package org.chreso.edots;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO: implement login loic
+                openDashboard();
+            }
+        });
+    }
+
+    private void openDashboard() {
+        Intent intent = new Intent(this, ClientMain.class);
+        startActivity(intent);
+    }
+}
